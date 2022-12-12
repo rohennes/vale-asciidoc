43c26aec18a9d6320086180df8240cfd04d86203
Aidan Reilly <74046732+aireilly@users.noreply.github.com>
2022-12-12 17:27:56 +0000

    Update README.md

--- scripts/validate-vale-rules.sh
+++ scripts/validate-vale-rules.sh
@@ -8,32 +8,32 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 # Fail on errors
+
 set -e
 
 Rule() {
     DIR=".vale/fixtures/AsciiDoc/$RULE"
     echo $DIR
     VALIDALERTSCOUNT="$(vale --config="$DIR/.vale.ini" --no-exit --output=line "$DIR/testvalid.adoc" | wc -l)"
-    INVALIDALERTS="$(vale --config="$DIR/.vale.ini" --no-exit --output=line "$DIR/testinvalid.adoc" | wc -l)"
-    #echo "Vale ALERTS: $INVALIDALERTS"
+    INVALIDALERTSCOUNT="$(vale --config="$DIR/.vale.ini" --no-exit --output=line "$DIR/testinvalid.adoc" | wc -l)"
     INVALIDLINES="$(grep -c "//vale-fixture" "$DIR/testinvalid.adoc" || true)"
     #echo "INVALID LINES: $INVALIDLINES"
-    INVALIDGAP=$(($INVALIDLINES - $INVALIDALERTS))
+    #echo "Vale ALERTS: $INVALIDALERTSCOUNT"
+    #INVALIDLINES="$(grep -cve '.+' "$DIR/testinvalid.adoc" || true)"
+    INVALIDGAP=$(($INVALIDALERTSCOUNT - $INVALIDLINES))
     if [ "$VALIDALERTSCOUNT" -gt 0 ]
     then
-    #    echo "INVALID LINES: $INVALIDLINES"
-    #    echo "Vale ALERTS: $INVALIDALERTS"
-        echo "ERROR: $VALIDALERTSCOUNT in .vale/fixtures/AsciiDoc/$RULE/testvalid.adoc for .vale/styles/AsciiDoc/$RULE.yml"
+        echo "$VALIDALERTSCOUNT ERROR(s) in .vale/fixtures/AsciiDoc/$RULE/testvalid.adoc for .vale/styles/AsciiDoc/$RULE.yml"
         TOTAL=$(( TOTAL + VALIDALERTSCOUNT ))
     fi
     if [ $INVALIDGAP -gt 0 ]
     then
-        echo "ERROR: $INVALIDGAP in .vale/fixtures/AsciiDoc/$RULE/testinvalid.adoc / .vale/styles/AsciiDoc/$RULE.yml"
+        echo "$INVALIDGAP ERROR(s) in .vale/fixtures/AsciiDoc/$RULE/testinvalid.adoc / .vale/styles/AsciiDoc/$RULE.yml"
         TOTAL=$(( TOTAL + INVALIDGAP ))
     fi
 }
 
-# This scripts runs the  suite for each rule in the `RedHat` style.
+# This scripts runs the  suite for each rule in the `AsciiDoc` style.
 TOTAL=0
 for RULE in $(find .vale/styles/AsciiDoc/ -name '*.yml' | cut -d/ -f 4 | cut -d. -f1 | sort)
 do  
